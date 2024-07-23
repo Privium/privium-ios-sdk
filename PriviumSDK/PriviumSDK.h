1cec6d5d82f50b650edfdd02b6cf808e957ad5bc
Ryan <ryan@skape.us>
2024-07-22 23:36:26 -0400

    Initial Commit

---
+++ PriviumSDK/PriviumSDK.h
@@ -0,0 +1,18 @@
+//
+//  PriviumSDK.h
+//  PriviumSDK
+//
+//  Created by Ryan Napolitano on 7/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PriviumSDK.
+FOUNDATION_EXPORT double PriviumSDKVersionNumber;
+
+//! Project version string for PriviumSDK.
+FOUNDATION_EXPORT const unsigned char PriviumSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PriviumSDK/PublicHeader.h>
+
+
